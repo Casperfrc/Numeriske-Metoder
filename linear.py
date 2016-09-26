96298dcb440c041d7552f76a81e306e2b2984a06
Areedtz <anders-sparrevohn@hotmail.com>
2016-09-26 12:59:31 +0200

    Added x*y list
    Made a while loop that makes a list full of x*y numbers

--- linear.py
+++ linear.py
@@ -7,6 +7,12 @@ yValues = input("Hvad er dine y-værdier?: ")
 yValuesList = [float(s) for s in re.findall(r"[-+]*\d*\.\d+|[-+]?\d*\d+", yValues)]
 
 x2ValuesList = []
+xyValuesList = []
 
 for i in xValuesList:
     x2ValuesList.append(i**2)
+
+w = 0
+while w < len(xValuesList):
+    xyValuesList.append(xValuesList[w]*yValuesList[w])
+    w += 1
