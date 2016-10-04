9e2b20476d0bcd71d9bce3fcdcdb9e91e47c4e41
Areedtz <anders-sparrevohn@hotmail.com>
2016-10-04 11:24:05 +0200

    Added differentation and calculation of it

--- differentation.py
+++ differentation.py
@@ -1,4 +1,8 @@
+from sympy.parsing.sympy_parser import parse_expr
+import sympy
+
 # Skal have fundet en bedre måde end eval
+# Kode skal fixes, så det generelt bliver mere effektivt
 
 def getInputs():
     # Lav noget, så man kan skrive x^3 i stedet for x**3   
@@ -14,3 +18,10 @@ def calcStuff(eq, h, x0, xmh, xph):
     fxmh = eval(eq.replace("x", xmh))
     fxph = eval(eq.replace("x", xph))
     fMark = (fxph - fxmh) / (2*(float(h)))
+    print(fMark)
+    eq = parse_expr(eq)
+    fPrime = eq.diff(x)
+    fPrime = eval(str(fPrime).replace("x", x0))
+    print(fPrime)
+
+x = sympy.Symbol("x")
